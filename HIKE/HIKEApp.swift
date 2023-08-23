d4f856abb551ae6a7ce87bae10c44dedde3a197c
daniele-pipino <81187045+daniele-pipino@users.noreply.github.com>
2023-08-23 15:53:08 +0200

    Initial Commit

---
+++ HIKE/HIKEApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIKEApp.swift
+//  HIKE
+//
+//  Created by Daniele Pipino on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HIKEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
